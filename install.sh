e47a004ca24bc05cb762db83a3e5e382a8559044
Frankie Robertson <frankie.robertson@gmail.com>
2013-10-26 01:13:55 +0100

    Added awesome config

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-#!/bin/zsh
-setopt extendedglob
-for f (${0%/*}/^install.sh) ln -s $f:a ~/.$f:t
+ln -s vimrc.before.local ~/.vimrc.before.local
+ln -s vimrc.bundles ~/.vimrc.bundles
+ln -s vimrc.local ~/.vimrc.local
+ln -s xinitrc ~/.xinitrc
+ln -s Xresources ~/.Xresources
+ln -s zshrc ~/.zshrc
+ln -s awesome ~/.config/awesome
