13c75fe8a52bdf294e3d283b486db669316096be
Frankie Robertson <frankie@robertson.name>
2015-01-27 13:52:05 +0200

    Install sh => py

---
+++ install.py
@@ -0,0 +1,30 @@
+from shutil import *
+from os import *
+
+pj = path.join
+rel = lambda *x: pj(path.abspath(path.dirname(__file__)), *x)
+
+def gohome(fn, extra=""):
+    fdest = extra + fn
+    symlink(rel(fn), pj("~", fdest))
+
+
+for f in [
+    "vimrc.before.local",
+    "vimrc.bundles.local",
+    "vimrc.local",
+]:
+    gohome(f)
+
+for f in [
+    "xinitrc",
+    "Xresources",
+    "zshrc",
+    "gitignore",
+    "gitconfig
+]:
+    gohome(f, ".")
+
+fontsd = "~/.fonts/"
+mkdir(fontsd)
+gohome(listdir(fontsd)[0], fontsd)
