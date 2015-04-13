17f0bdc7bba8a53e79c760d9e43896625e7bace0
Frankie Robertson <frankie@robertson.name>
2015-04-13 13:42:43 +0300

    Fix install.py

--- install.py
+++ install.py
@@ -1,32 +1,40 @@
 from shutil import *
 from os import *
+from os.path import expanduser
 
 pj = path.join
 rel = lambda *x: pj(path.abspath(path.dirname(__file__)), *x)
 
 def gohome(fn, extra=""):
     fdest = extra + fn
-    symlink(rel(fn), pj("~", fdest))
+    src = rel(fn)
+    dest = expanduser(pj("~", fdest))
+    print "symlink", src, dest
+    try:
+        symlink(src, dest)
+    except OSError:
+        pass
 
 
-for f in [
-    "vimrc.before.local",
-    "vimrc.bundles.local",
-    "vimrc.local",
-]:
+for f in []:
     gohome(f)
 
 for f in [
+    "nvimrc",
     "xinitrc",
     "Xresources",
     "zshrc",
     "gitignore",
-    "gitconfig"
+    "gitconfig",
+    "xmodmap"
 ]:
     gohome(f, ".")
 
-fontsd = "~/.fonts/"
-mkdir(fontsd)
+fontsd = expanduser("~/.fonts/")
+try:
+    mkdir(fontsd)
+except OSError:
+    pass
 gohome(listdir(fontsd)[0], fontsd)
 
 
